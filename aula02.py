e60aef31b83bfddd57693758cdfdc7e53093acdf
Nicolassousa-jpg <contatonicolassousa@gmail.com>
2024-12-03 09:54:01 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,22 @@
+print("hello user")
+
+user_name = "Nick" 
+# tipo -> str -> String
+
+idade_user = 19
+# tipo -> int -> integer
+
+Altura_user = 1.85
+# tipo -> float -> ponto flutuante
+
+maior_idade = True
+# tipo -> bool = boolean
+
+
+
+
+
+print ("seja bem vindo",user_name)
+print ("Sua idade é", idade_user)
+print ("sua altura é de", Altura_user, "metros.")
+print ("voce é maior de idade?",maior_idade)
