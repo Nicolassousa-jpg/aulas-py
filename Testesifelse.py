07ab99c255165d307bdf382cabeaee4224347224
Nicolassousa-jpg <contatonicolassousa@gmail.com>
2024-12-07 13:34:40 -0300

    Add files via upload

---
+++ Testesifelse.py
@@ -0,0 +1,16 @@
+print("hello user")
+
+user_name = "Nicolas Sousa"
+
+idade_user = 19
+
+maior_idade = True
+
+if idade_user < 18:
+    maior_idade = False
+    print("voce é menor de idade:",idade_user,"anos de idade")
+    
+
+else: 
+    maior_idade = True
+    print("voce é maior de idade:", idade_user, "anos de idade")
